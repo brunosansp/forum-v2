d7acf2165d383bc2a4702a8fc4f326e12a334c49
Bruno Santos <brunosansp@outlook.com>
2020-06-07 16:46:12 -0300

    Habilitando Spring Security e acesso aos endpoints

--- src/main/java/br/com/alura/forum/modelo/Usuario.java
+++ src/main/java/br/com/alura/forum/modelo/Usuario.java
@@ -1,18 +1,37 @@
 package br.com.alura.forum.modelo;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails {
 
+	private static final long serialVersionUID = 1L;
+	
 	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String nome;
 	private String email;
 	private String senha;
+	
+	@ManyToMany(fetch = FetchType.EAGER)
+	private List<Perfil> perfis = new ArrayList<>();
 
 	@Override
 	public int hashCode() {
@@ -39,36 +58,39 @@ public class Usuario {
 		return true;
 	}
 
-	public Long getId() {
-		return id;
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return this.perfis ;
 	}
 
-	public void setId(Long id) {
-		this.id = id;
+	@Override
+	public String getPassword() {
+		return this.senha;
 	}
 
-	public String getNome() {
-		return nome;
+	@Override
+	public String getUsername() {
+		return this.email;
 	}
 
-	public void setNome(String nome) {
-		this.nome = nome;
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
 	}
 
-	public String getEmail() {
-		return email;
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
 	}
 
-	public String getSenha() {
-		return senha;
-	}
-
-	public void setSenha(String senha) {
-		this.senha = senha;
+	@Override
+	public boolean isEnabled() {
+		return true;
 	}
 
 }
