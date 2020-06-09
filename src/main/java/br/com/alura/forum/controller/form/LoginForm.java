9a92f1d6f3dc6fd091ac60671c0875c77e7b8106
Bruno Santos <bruno.santoss@originalapp.com.br>
2020-06-09 09:39:37 -0300

    Configurando autenticacao stateless

---
+++ src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -0,0 +1,12 @@
+package br.com.alura.forum.controller.form;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class LoginForm {
+
+    private String email;
+    private String senha;
+
+}
