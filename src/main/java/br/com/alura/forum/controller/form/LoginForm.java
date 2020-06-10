54143e0d078563a947309b42e2e2db697fabd48f
Bruno Santos <brunosansp@outlook.com>
2020-06-10 08:11:36 -0300

    Gerando token com JWT

--- src/main/java/br/com/alura/forum/controller/form/LoginForm.java
+++ src/main/java/br/com/alura/forum/controller/form/LoginForm.java
@@ -1,5 +1,7 @@
 package br.com.alura.forum.controller.form;
 
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -8,5 +10,9 @@ public class LoginForm {
 
     private String email;
     private String senha;
+
+	public UsernamePasswordAuthenticationToken converter() {
+		return new UsernamePasswordAuthenticationToken(email, senha);
+	}
 
 }
